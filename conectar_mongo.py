f2386f60acaa5ff4a493a507e1c9d5af32d2b4f1
Chayanne Salazar <chayanne.morango02@gmail.com>
2024-02-22 11:52:27 -0300

    Desenvolvendo Janelas Graficas com banco de dados

---
+++ conectar_mongo.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://root:senac123@127.0.0.1:37452/")
+
+
+# selecionando o banco de dados loja_db
+db = client.loja_db
+
+# Estamos obtendo os dados que estão cadastrado na tabela(coleção) usuario,
+# usamos db [""].find().
+# O comando find localiza os dados e retorna com todos eles para a variável us.
+# Depois fizemos a leitura de todas linha com o for e exibimos na tela.
+
+
+#for us in db["usuario"].find():
+#   print (us)
+
+# Abaixo a consulta realiza o cadastro de um novo usuario e 
+# retorna o id do usuario cadastrado
+#usuario_id = db["usuario"].insert_one({"nomeusuario":"jude","senha":"123","nivel":"usuario"}).inserted_id
+#print(usuario_id)
+
+
+# Localizar apenas um usuário no banco dados
+#rs = db["usuario"].find_one({"nivel":"usuario"})
+#print(rs)
+
+# Localizar todos os dados do nível de acesso usuario
+#for rs in db["usuario"].find({"nivel":"usuario"}):
+#    print(rs)
